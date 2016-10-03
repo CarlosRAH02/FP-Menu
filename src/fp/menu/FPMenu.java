67777364d6a84baa8eca6e83fec52613f079ac4e
Samsung <Samsung@Samsung-PC>
2016-10-03 12:27:44 -0500

    FP - Menu. Muestra de menus consola en JAVA

--- src/fp/menu/FPMenu.java
+++ src/fp/menu/FPMenu.java
@@ -15,11 +15,15 @@ public class FPMenu {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // Menu en consola JAVA
+        
+        System.out.println("La opcion elegida es:" + menu());
+        
     }
     
     public static int menu (){
         int opcion;
+        do{
         Scanner entrada = new Scanner (System.in);
         System.out.println("**** Cálculo de Areas ****");
         System.out.println(" **** Menú: ****");
@@ -27,6 +31,30 @@ public class FPMenu {
         System.out.println("1.- Area de cuadrados");
         System.out.println("2.- Area de circulos");
         System.out.println("3.- Area de triangulos");
+        System.out.println("0.- Salir");
+        opcion = entrada.nextInt();
+        
+        }while (opcion<0 || opcion>3);
+        return opcion;
+    }
+    
+    public static double calcularAreas (int op){
+        double area;
+        switch (op){
+            case 1:
+                area = areaCuadrado();
+                break;
+            case 2:
+                area = areaCirculo();
+                break;
+            case 3:
+                area = areaTriangulo();
+                break;
+            default:
+                area = mensajeSalida();
+                
+        }
+        return area;
     }
 }
 
